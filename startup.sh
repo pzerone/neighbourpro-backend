6b95fc7eac62851fb8349533e78c03b08fc38490
Sreejith IV <46400271+pzerone@users.noreply.github.com>
2023-12-23 21:47:57 +0530

    Added documentation for startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+# migration files are inside the app/database folder
 cd app/database
+
+# apply database migrations using Aerich
 aerich upgrade
+
 cd ..
+# Start the Uvicorn server with the FastAPI app. hot reload is for development only
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload --log-level info
