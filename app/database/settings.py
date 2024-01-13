92ba965a0f6494becb27cd3623748abdc0563bb7
Sreejith IV <46400271+pzerone@users.noreply.github.com>
2024-01-13 22:11:24 +0530

    Use proper naming convention for db url

--- app/database/settings.py
+++ app/database/settings.py
@@ -2,10 +2,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-db_url = os.environ["DB_URL"]
+DB_URL = os.environ["DB_URL"]
 
 TORTOISE_ORM = {
-    "connections": {"default": db_url},
+    "connections": {"default": DB_URL},
     "apps": {
         "models": {
             "models": ["aerich.models", "models"],
