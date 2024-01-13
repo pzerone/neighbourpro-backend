b4dcc5dd5cc29c6309567f1fcd57b363d1790a2c
Sreejith IV <46400271+pzerone@users.noreply.github.com>
2024-01-13 12:20:22 +0530

    Added error handling and server run mode flags
    script now takes in [dev/prod] to run the server in development mode or
    production mode

--- startup.sh
+++ startup.sh
@@ -1,10 +1,31 @@
-#!/bin/sh
-# migration files are inside the app/database folder
+#!/bin/bash
+
+set -e
+
+# Check if a command line argument is provided
+if [ $# -eq 0 ]; then
+    echo -e "\e[91mError: Please provide a command line argument ('dev' or 'prod')\e[0m" >&2
+    exit 1
+fi
+
+# Set the migration files directory
 cd app/database
 
-# apply database migrations using Aerich
-aerich upgrade
+# Apply database migrations using Aerich
+# Returns early with failure if db connection fails
+if ! aerich upgrade > /dev/null 2>&1; then
+    echo -e "\e[91mError: Database migration failed. Check if the database is up and running\e[0m" >&2
+    exit 1
+fi
 
 cd ..
-# Start the Uvicorn server with the FastAPI app. hot reload is for development only
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload --log-level info
+
+# Determine whether to use --reload based on the command line argument
+if [ "$1" == "dev" ]; then
+    uvicorn main:app --host 0.0.0.0 --port 8000 --reload --log-level info
+elif [ "$1" == "prod" ]; then
+    uvicorn main:app --host 0.0.0.0 --port 8000 --log-level info
+else
+    echo -e "\e[91mError: Unknown parameter. Please use 'dev' or 'prod'\e[0m" >&2
+    exit 1
+fi
