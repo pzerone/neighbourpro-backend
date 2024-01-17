9684fc2dae6a3d2b013a6b3b5a5d477c0b346e75
Sreejith IV <46400271+pzerone@users.noreply.github.com>
2024-01-17 20:24:38 +0530

    Reordered tortoise models to get better IDE support

--- app/database/models.py
+++ app/database/models.py
@@ -12,6 +12,26 @@ from tortoise.contrib.postgres.fields import ArrayField
 # TODO: Add user_avg_rating field to Users model for workers
 
 
+class Professions(models.Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=20, unique=True)
+    description = fields.TextField(null=True)
+    estimated_time_hours = fields.FloatField(null=True)
+
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
+
+    # created_by = fields.ForeignKeyField(                                #
+    #     "models.Users", related_name="professions", null=False          #
+    # )                                                                   # TODO: Fix cyclic foreign key reference
+    # modified_by = fields.ForeignKeyField(                               # Add an extra table for these metadata to break the cyclic reference
+    #     "models.Users", related_name="professions_modified", null=False #
+    # )
+
+    class PydanticMeta:
+        exclude = ["id", "created_at", "modified_at"]
+
+
 class Users(models.Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(max_length=20, unique=True)
@@ -19,7 +39,7 @@ class Users(models.Model):
     last_name = fields.CharField(max_length=50, null=True)
 
     role = fields.CharField(max_length=10, null=False, default="user")
-    profession = fields.ForeignKeyField(
+    profession: fields.ForeignKeyRelation[Professions] = fields.ForeignKeyField(
         "models.Professions", related_name="profession", null=True
     )
     hourly_rate = fields.FloatField(null=True)
@@ -51,26 +71,6 @@ class Users(models.Model):
         exclude = ["created_at", "modified_at"]
 
 
-class Professions(models.Model):
-    id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=20, unique=True)
-    description = fields.TextField(null=True)
-    estimated_time_hours = fields.FloatField(null=True)
-
-    created_at = fields.DatetimeField(auto_now_add=True)
-    modified_at = fields.DatetimeField(auto_now=True)
-
-    # created_by = fields.ForeignKeyField(                                #
-    #     "models.Users", related_name="professions", null=False          #
-    # )                                                                   # TODO: Fix cyclic foreign key reference
-    # modified_by = fields.ForeignKeyField(                               #
-    #     "models.Users", related_name="professions_modified", null=False #
-    # )
-
-    class PydanticMeta:
-        exclude = ["id", "created_at", "modified_at"]
-
-
 class Works(models.Model):
     id = fields.IntField(pk=True)
     tags = ArrayField(null=True, element_type="text")
@@ -78,13 +78,13 @@ class Works(models.Model):
     scheduled_date = fields.DateField(null=True)
     scheduled_time = fields.TimeField(null=True)
 
-    booked_by = fields.ForeignKeyField(
+    booked_by: fields.ForeignKeyRelation[Users] = fields.ForeignKeyField(
         "models.Users", related_name="booked_by", null=False
     )
-    assigned_to = fields.ForeignKeyField(
+    assigned_to: fields.ForeignKeyRelation[Users] = fields.ForeignKeyField(
         "models.Users", related_name="assigned_to", null=False
     )
-    profession = fields.ForeignKeyField(
+    profession: fields.ForeignKeyRelation[Professions] = fields.ForeignKeyField(
         "models.Professions", related_name="profession_type", null=False
     )
     status = fields.CharField(max_length=20, null=False, default="pending")
@@ -108,8 +108,10 @@ class Reviews(models.Model):
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
 
-    user_id = fields.ForeignKeyField("models.Users", related_name="user_id", null=False)
-    worker_id = fields.ForeignKeyField(
+    user_id: fields.ForeignKeyRelation[Users] = fields.ForeignKeyField(
+        "models.Users", related_name="user_id", null=False
+    )
+    worker_id: fields.ForeignKeyRelation[Users] = fields.ForeignKeyField(
         "models.Users", related_name="worker_id", null=False
     )
 
